58e1d99666926adb354bf768666522ea4bf20923
Sandro Scalco <scalpo@bluewin.ch>
2014-10-02 23:47:14 +0200

    config chcanged

--- js/controllers.js
+++ js/controllers.js
@@ -8,15 +8,4 @@ angular.module('starter.controllers', [])
     // err.status will contain the status code
   })
 
-})
-
-.controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
-})
-
-.controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
-})
-
-.controller('AccountCtrl', function($scope) {
 });
