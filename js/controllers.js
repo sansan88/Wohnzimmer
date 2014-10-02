13c9b68c508173a666199f75fb6890321a14b112
Sandro Scalco <scalpo@bluewin.ch>
2014-10-03 00:04:38 +0200

    refresh

--- js/controllers.js
+++ js/controllers.js
@@ -3,9 +3,19 @@ angular.module('starter.controllers', [])
 .controller('DashCtrl', function($scope, $http) {
 	$http.get('http://sandroscalco.dyndns.org:3000').then(function(resp) {
     $scope.conditions = resp.data.temperatur;
-  }, function(err) {
-    console.error('ERR', err);
-    // err.status will contain the status code
-  })
+	  }, function(err) {
+	    console.error('ERR', err);
+	    // err.status will contain the status code
+	  })
+
+ $scope.refresh = function() {
+ 	$http.get('http://sandroscalco.dyndns.org:3000').then(function(resp) {
+    $scope.conditions = resp.data.temperatur;
+	  }, function(err) {
+	    console.error('ERR', err);
+	    // err.status will contain the status code
+	  })
+ };
+
 
 });
