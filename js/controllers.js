4d3033fcafb093756433a3906374dbbad9d8e798
Sandro Scalco <scalpo@bluewin.ch>
2014-10-02 23:38:13 +0200

    nochmals ein test

--- js/controllers.js
+++ js/controllers.js
@@ -2,7 +2,7 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope, $http) {
 	$http.get('http://sandroscalco.dyndns.org:3000').then(function(resp) {
-    $scope.conditions = resp.data.conditions;
+    $scope.conditions = resp.data.temperatur;
   }, function(err) {
     console.error('ERR', err);
     // err.status will contain the status code
