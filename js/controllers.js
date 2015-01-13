735322cf9ab479b0a2a08cda11e87b64fcd1f390
Sandro Scalco <info@sandrodesign.ch>
2015-01-13 18:11:06 +0100

    message

--- js/controllers.js
+++ js/controllers.js
@@ -1,25 +1,33 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $http) {
+.controller('DashCtrl', function($scope, $http, $ionicLoading) {
 	$scope.conditions = "..updating..";
   $scope.doRefreshDash = function() {
- 	$http.get('http://sandroscalco.dyndns.org:3000').then(function(resp) {
-    $scope.conditions = resp.data.temperatur;
-	  }, function(err) {
-			$scope.conditions = "-";
-	    //console.error('ERR', err);
-	    // err.status will contain the status code
-	  }); // end get
+		$ionicLoading.show({
+			template: 'loading'
+		})
+	 	$http.get('http://sandroscalco.dyndns.org:3000').then(function(resp) {
+	    $scope.conditions = resp.data.temperatur;
+		  }, function(err) {
+				$scope.conditions = "-";
+				$ionicLoading.hide();
+		    //console.error('ERR', err);
+		    // err.status will contain the status code
+		  }); // end get
+			$ionicLoading.hide();
  	};
 	$scope.doRefreshDash();
 })
 
 
-.controller('SchweizCtrl', function($scope, $http) {
+.controller('SchweizCtrl', function($scope, $http, $ionicLoading) {
 	//init
 	$scope.wetterdaten = [];
 
 	$scope.doRefreshMesswerte = function() {
+		$ionicLoading.show({
+			template: 'loading'
+		})
 		$http.jsonp('http://sandroscalco.dyndns.org:3000/wetterdaten/ch/?callback=JSON_CALLBACK')
 		.success(function(data){
 			$scope.wetterdaten_schweiz = JSON.parse(JSON.parse(data));
@@ -29,28 +37,39 @@ angular.module('starter.controllers', [])
 		.finally(function() {
 			// Stop the ion-refresher from spinning
 			$scope.$broadcast('scroll.refreshComplete');
+			$ionicLoading.hide();
 		});
 	};
 	$scope.doRefreshMesswerte();
 })
 
+.controller('WebcamCtrl', function($scope, $http, $ionicLoading) {
 
-.controller('WebcamCtrl', function($scope, $http) {
 
 	$scope.doRefreshWebcam = function(){
+
+		$ionicLoading.show({
+			template: 'loading'
+		})
 		$http.jsonp('http://sandroscalco.dyndns.org:3000/camera/?callback=JSON_CALLBACK')
 		.success(function(data){
 			//var camobj = JSON.parse(data);
 			$scope.webcam = "data:image/jpg;base64," + data.image;//camera;
+			window.localStorage.setItem("image", data.image);
 		})
 		.finally(function() {
+			$ionicLoading.hide();
 			// Stop the ion-refresher from spinning
 		//	$scope.$broadcast('scroll.refreshComplete');
 		});
 	};
 
 	if ( $scope.webcam === undefined ){
-		$scope.doRefreshWebcam();
+		if (window.localStorage.getItem("image") === null){
+			$scope.doRefreshWebcam();
+		}else{
+			$scope.webcam = "data:image/jpg;base64," + window.localStorage.getItem("image");
+		}
 	}
 
 });
